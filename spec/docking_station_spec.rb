e401fcda693f15d396c531b0d3b7df7085a4017f
Jen Strong <jenniferastrong@gmail.com>
2018-05-16 15:06:01 +0100

    challenge 10 - making stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require 'DockingStation'
+require 'Bike'
 
 describe DockingStation do
   it 'releases a bike' do
     expect(subject).to respond_to(:release_bike)
+  end
+
+  it 'returns a working bike' do
+    bike = subject.release_bike
+    expect(bike.class).to eq Bike
+    expect(bike.working?).to eq true
   end
 end
