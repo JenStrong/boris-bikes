54262670d98a995bf351bf26443d4c1765090b32
Jen Strong <jenniferastrong@gmail.com>
2018-05-16 16:51:49 +0100

    challenge 12: raising exceptions

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -3,9 +3,12 @@ require_relative 'Bike'
 class DockingStation
   attr_reader :bike_docked
 
-
   def release_bike
-    Bike.new
+    fail 'No bikes to be released' if @bike_docked.nil?
+    available_bikes = nil
+    available_bikes = @bike_docked
+    @bike_docked = nil #now there aren't any bikes
+    return available_bikes
   end
 
   def dock (bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,27 +2,26 @@ require 'DockingStation'
 require 'Bike'
 
 describe DockingStation do
-  it 'releases a bike' do
+  it 'can the docking station release a bike?' do
     expect(subject).to respond_to(:release_bike)
   end
 
   it 'returns a working bike' do
-    bike = subject.release_bike
-    expect(bike.class).to eq Bike
-    expect(bike.working?).to eq true
+    subject.dock(Bike.new)
+    bike = subject.release_bike #this is the output of the release_bike method (the acutal bike we're getting)
+    expect(bike.class).to eq Bike #expect the bike that's returned to be an object of the bike class.
+    expect(bike.working?).to eq true #this is to check that the bike is working
   end
 
   it 'check can you dock a bike' do
-    expect(subject).to respond_to(:dock_a_bike)
-    a = Bike.new
-    expect(subject.dock_a_bike(a)).to eq "You docked the bike!"
-    expect(subject.bike_docked).to eq a
-
+    expect(subject).to respond_to(:dock) #check whether the dock function exists
+    a = Bike.new #this is a new instance of the bike class (for example, Azlan's bike)
+    expect(subject.dock(a)).to eq "You docked the bike!" #expect when we input a to the docking function within a new instance of the docking station, for it to return this string.
+    expect(subject.bike_docked).to eq a #expect that the bike that's returned is actually Azlan's bike
   end
 
-
-
-
-
+  it 'raising an error for releasing a bike from an empty dock' do
+    expect{subject.release_bike}.to raise_error('No bikes to be released') #then when we try to relase a second bike, there isn't one there, so we want to check that an error message is being raised.
+  end
 
 end
