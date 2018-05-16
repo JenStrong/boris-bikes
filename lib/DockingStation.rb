d7a432c8acea0bc6c01ed29e8dd16edd74f63b88
Jen Strong <jenniferastrong@gmail.com>
2018-05-16 15:40:59 +0100

    challenge 11 - using instance variables

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,16 @@
 require_relative 'Bike'
 
 class DockingStation
+  attr_reader :bike_docked
+
+
   def release_bike
     Bike.new
   end
+
+  def dock (bike)
+    @bike_docked = bike
+    "You docked the bike!"
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,18 @@ describe DockingStation do
     expect(bike.class).to eq Bike
     expect(bike.working?).to eq true
   end
+
+  it 'check can you dock a bike' do
+    expect(subject).to respond_to(:dock_a_bike)
+    a = Bike.new
+    expect(subject.dock_a_bike(a)).to eq "You docked the bike!"
+    expect(subject.bike_docked).to eq a
+
+  end
+
+
+
+
+
+
 end
